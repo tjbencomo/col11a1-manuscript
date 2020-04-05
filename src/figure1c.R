f2abded5eff1fdf4e17fba19741d05a9a19d0b46
Tomas Bencomo <tjbencomo@gmail.com>
2020-04-04 19:51:46 -0700

    Add script to generate Figure1C and analyze helix breaking mutations in COL11A1

---
+++ src/figure1c.R
@@ -0,0 +1,109 @@
+# File: figure1c.R
+# Author: Tomas Bencomo
+# Description: Script generates Figure 1C. This script creates
+# a heatmap for COL11A1 Protein Isoform C. Isoform C differs from Isoform A by a deletion
+# from AA 261 - 299. While this deletion affects a nonhelical region,
+# the indexing for the other AAs in the protein are shifted leftward by 38 AAs
+# after the 260th AA position. get_region_extended() accounts for this shift.
+
+library(readr)
+library(dplyr)
+library(ggplot2)
+library(stringr)
+library(reshape2)
+# library(scales)
+# library(colorspace)
+# library(cowplot)
+
+
+get_region_extended <- function(position) {
+  if (position > 260) {
+    position = position + 38
+  }
+  if (position < 230) {
+    return("Pre-Nonhelical region")
+  } else if (position >= 230 && position <= 419) {
+    return("Triple-helical region (interrupted)")
+  } else if (position >= 509 && position <= 511) {
+    return("Short nonhelical segment")
+  } else if (position >= 512 && position <= 528) {
+    return("Telopeptide")
+  } else if (position >= 529 && position <= 1542) {
+    return("Triple-helical region")
+  } else if (position >= 1543 && position <= 1563) {
+    return("Nonhelical region (C-terminal)")
+  } else {
+    return("Fibrillar collagen NC1")
+  }
+  # if (position >= 0 && position < 71) {
+  #   return("Pre-Laminin G-like")
+  # } else if (position >= 71 && position <= 243) {
+  #   return('Laminin G-like')
+  # } else if (position > 243 && position < 529) {
+  #   return("Pre-Triple Helical")
+  # } else if(position >= 529 && position <= 1542) {
+  #   return('Triple Helical Region')
+  # } else if (position > 1542 && position < 1577) {
+  #   return("Pre-Fibrillar")
+  # } else if (position >= 1577 && position <= 1805) {
+  #   return('Fibrillar collagen NC1')
+  # } else {
+  #   return("Post-Fibrillar collagen NC1")
+  # }
+}
+
+
+snps <- read_delim("data/mutations.maf.gz", 
+                                "\t", escape_double = FALSE, trim_ws = TRUE, 
+                                col_types = c(
+                                  t_ref_count = "c", t_alt_count = "c", t_depth = "c", 
+                                  n_depth = "c", n_alt_count = "c", n_ref_count = "c"
+                                ))
+snps <- snps %>% filter(Hugo_Symbol == "COL11A1", Variant_Classification %in% c("Missense_Mutation",
+                                                                                "Nonsense_Mutation"))
+
+snps <- snps[complete.cases(snps$HGVSp_Short), ]
+extracted_aas <- snps$HGVSp_Short %>% str_extract_all("[A-Z]", simplify = T)
+extracted_pos <- snps$HGVSp_Short %>% str_extract("[0-9]+")
+snps$reference_aa <- extracted_aas[, 1]
+snps$variant_aa <- extracted_aas[, 2]
+snps$aa_position <- as.integer(extracted_pos)
+
+# snps$reference_aa <- sapply(snps$aachange, function(x) substr(x, 3,3))
+# snps$variant_aa <- sapply(snps$aachange, function(x) substr(x, nchar(x),nchar(x)))
+# snps$aa_position <- sapply(snps$aachange, function(x) str_extract(x, "([0-9]+)"))
+# snps$aa_position <- as.integer(snps$aa_position)
+snps$region <- sapply(snps$aa_position, get_region_extended)
+snps$pg.mutation <- ifelse(snps$reference_aa %in% c("P", "G") & !(snps$variant_aa %in% c("P", "G")), 1, 0)
+snps$other.mutation <- ifelse(snps$reference_aa %in% c("P", "G") & !(snps$variant_aa %in% c("P", "G")), 0, 1)
+
+# region.lengths <- c(71, 243 - 71, 529 - 244, 1542 - 529, 1576 - 1543, 1805 - 1577)
+# region.names <- c("Pre-Laminin G-like", "Laminin G-like", "Pre-Triple Helical", "Triple Helical Region",
+#                   "Pre-Fibrillar", "Fibrillar collagen NC1")
+
+region.lengths <- c(229, 419 - 230, 511-509, 528-512, 1542-529, 1563-1543, 1767-1564)
+region.names <- c("Pre-Nonhelical region", "Triple-helical region (interrupted)",
+                  "Short nonhelical segment", "Telopeptide", "Triple-helical region",
+                  "Nonhelical region (C-terminal)", "Fibrillar collagen NC1")
+
+snps$region <- factor(snps$region, levels = region.names)
+
+
+region.info <- data.frame(region = region.names, lengths = region.lengths)
+region.info$region <- as.character(region.names)
+
+counts <- snps %>% 
+  count(region, .drop = F) %>% 
+  inner_join(region.info) %>%
+  mutate(normalized.count = n / lengths)
+
+melted <- melt(select(counts, region, normalized.count), id.var = "region")
+
+p <- ggplot(melted, aes(variable, factor(region, levels = region.names))) + 
+  geom_tile(aes(fill = value), width = .4) +
+  coord_flip() +
+  scale_fill_distiller(palette = "Reds", direction = 1, name = "Mutations/AA", 
+                       limits = c(0, .15), breaks = c(0, .05, .1, .15)) +
+  labs(x = "", y = "Region")
+print(p)
+
---
+++ src/helix_breakers.R
@@ -0,0 +1,111 @@
+# File: helix_breakers.R
+# Author: Tomas Bencomo
+# Description: Calculate number of helix breaking mutations
+# in the triple helical region of COL11A1. A helix breaking
+# mutation is any amino acid change  in the triple-helical domain
+# that converts a Proline or Glycine to a different amino acid
+# that isn't a Proline or Glycine.
+
+library(readr)
+library(dplyr)
+library(stringr)
+
+# Position indices derived from Uniprot
+get_region <- function(position, transcript) {
+  # Correction for 38 AA deletion in Isoform C
+  if (str_detect(transcript, "ENST00000353414")) {
+    if (position > 260) {
+      position = position + 38
+    }
+  }
+  if (position >= 230 & position <= 419) {
+    return("Nonhelical region")
+  } else if (position >= 420 & position <= 508) {
+    return("Triple-helical region (interrupted)")
+  } else if (position >= 509 & position <= 511) {
+    return("Short nonhelical segment")
+  } else if (position >= 512 & position <= 528) {
+    return("Telopeptide")
+  } else if (position >= 529 & position <= 1542) {
+    return("Triple-helical region")
+  } else if (position >= 1543 & position <= 1563) {
+    return("Nonhelical region (C-terminal)")
+  } else {
+    return("None")
+  }
+}
+
+mutations <- read_delim("data/mutations.maf.gz", 
+                        "\t", escape_double = FALSE, trim_ws = TRUE, 
+                        col_types = c(
+                          t_ref_count = "c", t_alt_count = "c", t_depth = "c", 
+                          n_depth = "c", n_alt_count = "c", n_ref_count = "c"
+                        )) %>%
+  mutate(patient = factor(patient)) %>%
+  filter(Hugo_Symbol == "COL11A1") 
+  
+mutations <- mutations %>%
+  mutate(ref_aa = str_extract(HGVSp_Short, "[A-Z]"),
+         position = str_extract(HGVSp_Short, "[0-9]+"),
+         position = as.numeric(position),
+         mutant_aa = str_extract(str_sub(HGVSp_Short, 4), "[A-Z]+|\\*"),
+         mutant_aa = case_when(
+           str_detect(HGVSp_Short, "_splice") ~ "splice",
+           str_detect(HGVSp_Short, "=") ~ ref_aa,
+           TRUE ~ mutant_aa
+         )) %>%
+  filter(!is.na(HGVSp_Short))
+mutations$region <- apply(mutations[,c('position','Transcript_ID')], 1, 
+                          function(x) get_region(as.numeric(x['position']), 
+                                                 x['Transcript_ID']))
+mutations <- mutations %>%
+  mutate(
+    pg_mutation = case_when(
+      ref_aa %in% c("P", "G") & !(mutant_aa %in%c("P", "G")) ~ 1,
+      TRUE  ~ 0
+    ),
+    helix_breaking = case_when(
+      pg_mutation == 1 & region == "Triple-helical region" ~ 1,
+      TRUE ~ 0
+    )
+  )
+
+# mutations <- mutations %>%
+#   mutate(helix_breaker = case_when(
+#     ref_aa %in% c("P", "G") & !(mutant_aa %in%c("P", "G")) ~ 1,
+#     TRUE ~ 0
+#   ))
+
+# Plot where mutations fall in the amino acid sequence
+p <- mutations %>%
+  mutate(pg_mutation = case_when(
+    pg_mutation == 0 ~ "Other AA Mutations",
+    pg_mutation == 1 ~ "Proline/Glycine Mutation"
+  )) %>%
+  mutate(pg_mutation = factor(pg_mutation, levels=c("Other AA Mutations", "Proline/Glycine Mutation"))) %>%
+  ggplot(aes(position)) +
+  geom_histogram(aes(fill = factor(pg_mutation)), bins = 40) +
+  facet_grid(rows = vars(pg_mutation)) +
+  geom_vline(xintercept = c(529-38, 1542-38), color = "red", linetype = "dashed") + #Note adjustment for Isoform C
+  labs(x = "Amino Acid Position in COL11A1", y = "Number Of Mutations") +
+  scale_x_continuous(breaks = c(0, 500, 1000, 1500, 1800)) +
+  theme(legend.position = "none") +
+  annotate("rect", xmin=529-38, xmax=1542-38, ymin=0, ymax=Inf, alpha=0.2, fill="red")
+print(p)
+
+# Calculate number of SCCs with at least one helix breaking mutation
+# Helix breaking mutation is a P/G replacement in the triple-helical region
+prop.samples <- mutations %>%
+  group_by(patient, .drop = FALSE) %>%
+  summarise(n = sum(helix_breaking)) %>%
+  mutate(mutated = case_when(
+    n > 0 ~ 1,
+    n == 0 ~ 0
+  )) %>%
+  summarise(prop.broken.helix = sum(mutated) / 100)
+print(prop.samples)
+
+print(paste("Total Number of Proline/Glycine Mutations:", sum(mutations$pg_mutation)))
+print(paste("Proline/Glycine Mutations in Triple Helix Region:",
+            sum(mutations[mutations$region == "Triple-helical region", ]$helix_breaking)))
+  
